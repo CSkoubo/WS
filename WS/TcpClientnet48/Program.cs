4ad575824a65139b56e9deb2e7822bd5d7102423
Casper Skoubo <cssk@demant.com>
2022-05-11 08:12:13 +0200

    net48 app

---
+++ WS/TcpClientnet48/Program.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using System;
+using System.Threading.Tasks;
+
+namespace TcpClientnet48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating client");
+            DataClass sendDataClass = new DataClass { Number = 2, Text = "TextAAAAA" };
+            var serializer = new JsonSerializerWrapper();
+            var client = new ClientConnection(serializer, "192.168.1.83", 50001);
+            Console.WriteLine("client created");
+            Console.WriteLine("sending....");
+            Task.Run(async () => await client.SendObjectAsync(sendDataClass));
+            Console.WriteLine("sent.");
+        }
+    }
+}
+
+
+
